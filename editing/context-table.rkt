3d9b40e16931584dc95902cb480aed0937104a87
Daniel Feltey <dfeltey@gmail.com>
2016-06-30 17:03:51 -0500

    Only implement the mini-java relevant refactoring

--- editing/context-table.rkt
+++ editing/context-table.rkt
@@ -1,219 +1,51 @@
 #lang racket
 
-(provide build-context-table find-surrounding-context handle-expansion)
+(provide build-refactor-info handle-expansion)
 
 (require syntax/parse
          "property.rkt"
          "syntax-info.rkt")
 
-(define-syntax-class val
-  #:literal-sets (kernel-literals)
-  (pattern (~or :identifier
-                :boolean
-                :str
-                :char
-                :keyword
-                :number))
-  (pattern (quote qv:quoted-val))
-  (pattern (quote-syntax qv:quoted-val))
-  (pattern (quote-syntax qv:quoted-val #:local))
-  (pattern (#%plain-lambda formals expr ...))
-  (pattern (case-lambda (formals expr ...) ...))
-  (pattern (#%plain-app constructor:id v:val ...)
-           #:when (syntax-property #'constructor 'constructor-for))
-  ;; these are probably also values ...
-  (pattern (#%top . v:id))
-  (pattern (#%variable-reference v:id))
-  (pattern (#%variable-reference (#%top . v:id)))
-  (pattern (#%variable-reference)))
+;; Need to ensure non-false values are put in the refactor-table
+(struct wrap (prop) #:prefab)
 
-(define-syntax-class quoted-val
-  (pattern v:val)
-  (pattern ())
-  (pattern (qv:quoted-val ...)))
-
-(define value?
-  (syntax-parser
-    [:val #t]
-    [_ #f]))
-
-;; traverse fully expanded syntax and produce a hashtable
-;; that maps (list source position span) elements to
-;; (listof (list source position span)) or #f
-;; a mapping to #f indicates the syntax object is not immediately
-;; within an evaluation context so an if cannot be lifted
-(define (build-context-table stx)
-  (define parent-context-map (make-hash))
+;; traverse fully expanded syntax and produce a list suitable for building
+;; an interval-map. This maps intervals of if expressions to information
+;; about their syntax to swap the then/else branches and negate the test
+(define (build-refactor-info stx source)
   (define refactor-info (make-hash))
-  (define current-context-quoted? (make-parameter #f))
-  
-  (define (update-context-map this-loc [parent-loc #f])
-    (define current (hash-ref parent-context-map this-loc (set)))
-    (hash-set! parent-context-map
-               this-loc
-               (and parent-loc
-                    current
-                    (set-add current parent-loc))))
-  (define (add-sub-exprs stxs p-loc)
-    (for/fold ([E? #t])
-              ([expr (in-syntax stxs)])
-      (update-context-map (syntax-loc expr) (and E? p-loc))
-      (loop expr)
-      (value? expr)))
-  
-  (define (loop [stx stx])
-    ;; TODO: this may be too conservative in some cases
-    (hash-update! refactor-info
-                  (partial-syntax-loc stx)
-                  (λ (old)
-                    (define new-prop (get-refactor-property stx))
-                    (define new-span (syntax-span stx))
-                    (cond
-                      [old
-                       (match-define (cons old-span old-prop) old)
-                       ;; TODO: handle span differences here ...
-                       (cons old-span (and (equal? old-prop new-prop) old-prop))]
-                      [else (cons new-span new-prop)]))
-                  #f)
-    (define loc (and (not (current-context-quoted?)) (syntax-loc stx)))
-    (syntax-parse stx
-      #:literal-sets (kernel-literals)
-      [(#%expression expr)
-       (define expr-loc (syntax-loc #'expr))
-       (update-context-map (syntax-loc #'expr) loc)
-       (loop #'expr)]
-      [(module name mod-path
-         (#%plain-module-begin mod-level-form ...))
-       (for ([mod-level-form (in-syntax #'(mod-level-form ...))])
-         (update-context-map (syntax-loc mod-level-form))
-         (loop mod-level-form))]
-      [(module* name mod-path
-         (#%plain-module-begin mod-level-form ...))
-       (for ([mod-level-form (in-syntax #'(mod-level-form ...))])
-         (update-context-map (syntax-loc mod-level-form))
-         (loop mod-level-form))]
-      [(#%provide raw-provide-spec ...) (void)]
-      [(#%declare declaration-keyword ...) (void)]
-      [(define-values (id ...) expr)
-       (update-context-map (syntax-loc #'expr))
-       (loop #'expr)]
-      [(define-syntaxes (id ...) expr)
-       (update-context-map (syntax-loc #'expr))
-       (loop #'expr)]
-      [(#%require raw-require-spec ...) (void)]
-      [(#%plain-lambda formals expr ...)
-       (for ([expr (in-syntax #'(expr ...))])
-         (update-context-map (syntax-loc expr))
-         (loop expr))]
-      [(case-lambda (formals expr ...) ...)
-       (for ([expr (in-syntax #'(expr ... ...))])
-         (update-context-map (syntax-loc expr))
-         (loop expr))]
-      [(if test then else)
-       (update-context-map (syntax-loc #'test) loc)
-       (update-context-map (syntax-loc #'then))
-       (update-context-map (syntax-loc #'else))
-       (loop #'test)
-       (loop #'then)
-       (loop #'else)]   
-      [(set! id expr)
-       (update-context-map (syntax-loc #'expr) loc)
-       (loop #'expr)]
-      [(quote datum)
-       (update-context-map (syntax-loc #'datum))
-       (parameterize ([current-context-quoted? #t])
-         (loop #'datum))]
-      [(quote-syntax datum)
-       (update-context-map (syntax-loc #'datum))
-       (parameterize ([current-context-quoted? #t])
-         (loop #'datum))]
-      [(#%top . id) (void)]
-      [(#%variable-reference id) (void)]
-      [(#%variable-reference) (void)]
-      ;; All of the following forms must be treated specially as at most one subexpression
-      ;; within them is in an evaluation context
-      [(let-values ([(id ...) expr] ...) body ...)
-       (add-sub-exprs #'(expr ...) loc)
-       (for ([body (in-syntax #'(body ...))])
-         (update-context-map (syntax-loc body))
-         (loop body))]
-      [(letrec-values ([(id ...) expr] ...) body ...)
-       (add-sub-exprs #'(expr ...) loc)
-       (for ([body (in-syntax #'(body ...))])
-         (update-context-map (syntax-loc body))
-         (loop body))]
-      [(begin top-level-form ...)
-       (add-sub-exprs #'(top-level-form ...) loc)]
-      [(begin-for-syntax top-level-form ...)
-       (add-sub-exprs #'(top-level-form ...) loc)]
-      [(begin0 expr ...)
-       (add-sub-exprs #'(expr ...) loc)]
-      [(with-continuation-mark key val res)
-       (add-sub-exprs #'(key val res) loc)]
-      [(#%plain-app expr ...)
-       (add-sub-exprs #'(expr ...) loc)]
-      [(any ...)
-       (for ([any (in-syntax #'(any ...))])
-         (update-context-map (syntax-loc any) loc)
-         (loop any))]
-      [_ (void)]))
-  (loop)
-  (values
-   (for/hash ([(k v) (in-hash parent-context-map)])
-    (values k (and v (set->list v))))
-   refactor-info))
+  (let loop ([stx stx])
+    (define prop (get-refactor-property stx))
+    (when (and prop (equal? source (syntax-source stx)))
+      (hash-update! refactor-info
+                    (syntax-loc stx)
+                    (λ (old)
+                      (define new-span (syntax-span stx))
+                      (cond
+                        [old
+                         (match-define (wrap old-prop) old)
+                         (wrap (and (equal? old-prop prop) old-prop))]
+                        [else (wrap prop)]))
+                    #f))
+    (when (syntax->list stx)
+      (for ([sub-stx (in-syntax stx)])
+        (loop sub-stx))))
+  (build-pre-interval-map refactor-info))
 
-;; takes a context mapping and a location and finds the largest
-;; encompassing context
-;; assumes that loc has the original source
-(define (find-surrounding-context ctx-map loc)
-  (match-define (syntax-info source pos span) loc)
-  (let loop ([maybe-parent (hash-ref ctx-map loc #f)]
-             ;; initialize current to #f so that when there is no
-             ;; containing context, the option for lifting an if expression
-             ;; will be unavailable
-             [current #f])
-    (define parent (and maybe-parent (set->list maybe-parent)))
-    (cond
-      [(and parent (= 1 (length parent)))
-       (match-define (list (syntax-info p-source p-pos p-span)) parent)
-       (if (contains (first  parent) loc)
-           (loop (hash-ref ctx-map (first parent) #f) (first parent))
-           (loop (hash-ref ctx-map (first parent) #f) current))]
-      [parent
-       ;; TODO: This may be overly conservarive, but I'm not sure what
-       ;; the best thing to do is right now, so just disallow any refactorings
-       ;; in this situation.
-       ;; Returning current, returns the last largest containing context
-       current]
-      [else current])))
-
-(define (contains p-loc loc)
-  (match-define (syntax-info p-source p-pos p-span) p-loc)
-  (match-define (syntax-info source pos span) loc)
-  (and (equal? p-source source)
-       (<= p-pos pos)
-       (<= (+ pos span) (+ p-pos p-span))))
-
-
+(define (build-pre-interval-map table)
+  (sort (for*/list ([(k v) (in-hash table)]
+                    [prop (in-value (wrap-prop v))]
+                    #:when prop)
+          (match-define (syntax-info _ pos span) k)
+          (cons (cons (sub1 pos) (sub1 (+ pos span)))
+                prop))
+        (match-lambda**
+         [((cons start1 end1) (cons start2 end2))
+          (or (< start1 start2)
+              (and (= start1 start2)
+                   (>= end1 end2)))])
+        #:key car))
 
 (define (handle-expansion stx path source cust)
-  (cond
-    [(syntax? stx)
-     (define-values (ctx-table rf-table)
-       (build-context-table stx))
-     (list source ctx-table rf-table)]
-    [else #f]))
-
-
-  
-(require (for-syntax syntax/parse))
-(define-syntax (make-string stx)
-  (syntax-parse stx
-    [(_ body)
-     #'(with-output-to-string
-         (thunk (write 'body)))]))
-(define-syntax-rule (:expand stx)
-  (parameterize ([port-count-lines-enabled #t]
-                 [current-namespace (make-base-namespace)])
-    (expand (read-syntax (gensym) (open-input-string stx)))))
+  (and (syntax? stx)
+       (build-refactor-info stx source)))
--- editing/refactor-tool.rkt
+++ editing/refactor-tool.rkt
@@ -5,7 +5,8 @@
          racket/gui
          racket/runtime-path
          "context-table.rkt"
-         "syntax-info.rkt")
+         "syntax-info.rkt"
+         data/interval-map)
 
 (provide tool@)
 
@@ -19,32 +20,17 @@
         (define (phase2) (void))
 
         (define make-refactor-text%
-          (mixin (text:basic<%> racket:text<%>) ()
+          (mixin (racket:text<%>) ()
             (inherit begin-edit-sequence end-edit-sequence
                      insert delete)
 
-            (define context-table #f)
-            (define refactor-table #f)
-            (define source #f)
+            (define refactor-info #f)
 
-            (define/private (update-context-table! [table #f])
-              (set! context-table table))
-            (define/private (update-refactor-table! [table #f])
-              (set! refactor-table table))
-            (define/private (update-source! [src #f])
-              (set! source src))
+            (define/private (update-refactor-info! [info #f])
+              (set! refactor-info (and info (make-interval-map info))))
 
             (define/public (refactor-callback expanded-info)
-              (cond
-                [expanded-info
-                 (match-define (list source context-table refactor-table) expanded-info)
-                 (update-context-table! context-table)
-                 (update-refactor-table! refactor-table)
-                 (update-source! source)]
-                [else
-                 (update-context-table!)
-                 (update-refactor-table!)
-                 (update-source!)]))
+              (update-refactor-info! expanded-info))
 
             (define/public (refactor-build-popup-menu menu pos text)
               (define refactor-menu
@@ -52,85 +38,44 @@
                   "Refactor"
                   menu))
               (send refactor-menu enable #f)
-              (when (lift-refactoring-available? pos)
-                (new menu-item%
-                     [label "Lift if expression"]
-                     [parent refactor-menu]
-                     [callback (λ (item evt) (do-lift-if pos))])
-                (send refactor-menu enable #t))
-              (when (swap-then-else-refactoring-available? pos)
+              (when (get-refactor-info pos)
                 (new menu-item%
                      [label "Swap then/else branches"]
                      [parent refactor-menu]
                      [callback (λ (item evt) (do-swap-then-else pos))])
                 (send refactor-menu enable #t)))
 
-            (define/private (get-raw-refactor-info pos)
-              (and source
-                   refactor-table
-                   (hash-ref refactor-table (syntax-info source pos #f) #f)))
-
-            (define/private (lift-refactoring-available? pos)
-              (cond
-                [(get-raw-refactor-info pos)
-                 =>
-                 (λ (refactor-info)
-                   (match-define (cons span refactor-prop) refactor-info)
-                   (and refactor-prop context-table
-                        (find-surrounding-context context-table (syntax-info source pos span))))]
-                [else #f]))
-
-            (define/private (swap-then-else-refactoring-available? pos)
-              (cond
-                [(get-raw-refactor-info pos) => cdr]
-                [else #f]))
-
-            (define/private (do-lift-if pos)
-              (when context-table
-                (define refactor-info (get-raw-refactor-info pos))
-                (when refactor-info
-                  (match-define (cons span refactor-prop) refactor-info)
-                  (when refactor-prop
-                    (match-define (list lang if-loc test-loc then-loc else-loc) refactor-prop)
-                    (define loc (syntax-info source pos span))
-                    (define outer-context
-                      (find-surrounding-context context-table loc))
-                    (when outer-context
-                      (do-edit-if outer-context
-                                  if-loc
-                                  test-loc
-                                  then-loc
-                                  else-loc))))))
+            (define/private (get-refactor-info pos)
+              (and refactor-info
+                   (interval-map-ref refactor-info pos #f)))
 
             (define/private (do-swap-then-else pos)
-              (define refactor-info (get-raw-refactor-info pos))
+              (define refactor-info (get-refactor-info pos))
               (when refactor-info
-                (match-define (cons span refactor-prop) refactor-info)
-                (when refactor-prop
-                  (match-define (list lang if-loc test-loc then-loc else-loc) refactor-prop)
-                  (match-define (list test-start test-span) (shift test-loc))
-                  (match-define (list then-start then-span) (shift then-loc))
-                  (match-define (list else-start else-span) (shift else-loc))
-                  (begin-edit-sequence)
-                  (define then-text (copy-to-temp-text then-start (+ then-start then-span)))
-                  (define else-text (copy-to-temp-text else-start (+ else-start else-span)))
-                  (send this delete else-start (+ else-start else-span))
-                  (send then-text move/copy-to-edit
-                        this
-                        0
-                        then-span
-                        else-start
-                        #:try-to-move? #f)
-                  (send this delete then-start (+ then-start then-span))
-                  (send else-text move/copy-to-edit
-                        this
-                        0
-                        else-span
-                        then-start
-                        #:try-to-move? #f)
-                  (do-negate-test-edit test-start test-span lang)
-                  (end-edit-sequence)
-                  (void))))
+                (match-define (list lang if-loc test-loc then-loc else-loc) refactor-info)
+                (match-define (list test-start test-span) (shift test-loc))
+                (match-define (list then-start then-span) (shift then-loc))
+                (match-define (list else-start else-span) (shift else-loc))
+                (begin-edit-sequence)
+                (define then-text (copy-to-temp-text then-start (+ then-start then-span)))
+                (define else-text (copy-to-temp-text else-start (+ else-start else-span)))
+                (send this delete else-start (+ else-start else-span))
+                (send then-text move/copy-to-edit
+                      this
+                      0
+                      then-span
+                      else-start
+                      #:try-to-move? #f)
+                (send this delete then-start (+ then-start then-span))
+                (send else-text move/copy-to-edit
+                      this
+                      0
+                      else-span
+                      then-start
+                      #:try-to-move? #f)
+                (do-negate-test-edit test-start test-span lang)
+                (end-edit-sequence)
+                (void)))
 
             (define/private (get-negate-id lang)
               (case lang
@@ -148,50 +93,6 @@
                  (send this insert (string-append negate-id " " "(") start)])
               (end-edit-sequence))
 
-            (define/private (do-edit-if ctx-loc if-loc test-loc then-loc else-loc)
-              (match-define (list ctx-start ctx-span) (shift ctx-loc))
-              (match-define (list if-start if-span) (shift if-loc))
-              (match-define (list test-start test-span) (shift test-loc))
-              (match-define (list then-start then-span) (shift then-loc))
-              (match-define (list else-start else-span) (shift else-loc))
-
-              (begin-edit-sequence)
-              (define ctx-pre (copy-to-temp-text ctx-start if-start))
-              (define ctx-post (copy-to-temp-text (+ if-start if-span) (+ ctx-start ctx-span)))
-              ;; delete the post context
-              (send this delete (+ if-start if-span) (+ ctx-start ctx-span))
-              ;; put the post context after the else ....
-              (send ctx-post move/copy-to-edit
-                    this
-                    0
-                    (- (+ ctx-start ctx-span) (+ if-start if-span))
-                    (+ else-start else-span)
-                    #:try-to-move? #f)
-              ;; put the pre context before the else
-              (send ctx-pre move/copy-to-edit
-                    this
-                    0
-                    (- if-start ctx-start)
-                    else-start
-                    #:try-to-move? #f)
-              ;; put the post context after the then
-              (send ctx-post move/copy-to-edit
-                    this
-                    0
-                    (- (+ ctx-start ctx-span) (+ if-start if-span))
-                    (+ then-start then-span)
-                    #:try-to-move? #f)
-              ;; put the pre context before the then
-              (send ctx-pre move/copy-to-edit
-                    this
-                    0
-                    (- if-start ctx-start)
-                    then-start
-                    #:try-to-move? #f)
-              ;; delete the pre context
-              (send this delete ctx-start if-start)
-              (end-edit-sequence))
-
             (define/private (copy-to-temp-text start end)
               (define temp (new (text:basic-mixin (editor:basic-mixin text%))))
               (send this move/copy-to-edit temp start end 0 #:try-to-move? #f)
@@ -201,15 +102,15 @@
               (match-define (syntax-info _ pos span) loc)
               (list (sub1 pos) span))
 
-            (define/public (refactor-key-callback)
+            #;(define/public (refactor-key-callback)
               (define pos-box (box #f))
               (send this get-position pos-box)
               (define pos (unbox pos-box))
-              (do-lift-if pos))
+              (void))
 
             (super-new)))
 
-        (define (add-refactor-keybindings keymap)
+        #;(define (add-refactor-keybindings keymap)
           (send keymap add-function
                 "lift if expression"
                 (λ (obj evt)
@@ -224,7 +125,7 @@
              (when (and pos (is-a? text text%))
                (send editor refactor-build-popup-menu menu pos text)))))
 
-        (add-refactor-keybindings (drracket:rep:get-drs-bindings-keymap))
+        ;(add-refactor-keybindings (drracket:rep:get-drs-bindings-keymap))
         (drracket:get/extend:extend-definitions-text make-refactor-text%)
         (drracket:module-language-tools:add-online-expansion-handler
          context-table.rkt
