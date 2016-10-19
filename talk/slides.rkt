8b806640b326c57a4997380d3c0359945bdd1908
Daniel Feltey <dfeltey@gmail.com>
2016-10-19 13:52:58 -0500

    version 2 of reader/bindings slides

--- talk/slides.rkt
+++ talk/slides.rkt
@@ -3,8 +3,9 @@
 (require ppict
          "picts.rkt"
          "pslide-staged.rkt"
+         "racket-lwc-pict.rkt"
          "util.rkt"
-         pict/code
+         (except-in pict/code code)
          slideshow/text
          slideshow/play
          images/logos
@@ -14,7 +15,7 @@
 
 (require ppict-slide-grid)
 ;(pslide-base-pict (thunk (colorize (filled-rectangle client-w client-h) "Ivory")))
-(set-grid-base-pict!)
+;(set-grid-base-pict!)
 
 
 ;; LWC Talk Outline
@@ -26,66 +27,98 @@
  #:go (coord 0.5 0.33)
  (t "Languages the Racket Way"))
 
-(define (fade-to-ghost p n)
-  (fade-pict n p (ghost p)))
-(define (fade-from-ghost p n)
-  (fade-pict n (ghost p) p))
+
+(pslide
+ #:go (coord 0.5 0.5)
+ my-base-pict
+ #:go (coord 0.5 0.5)
+ giant-duck-pict)
 
 (play-n
- #:skip-last? #t
-(λ (n1 n2)
-  (define syntax (fade-pict n2 (label "syntax") (label "reader")))
-  (define semantics (fade-pict n2 (label "semantics") (label "bindings")))
-  (define syntax-ghost (launder (ghost (make-cloud "syntax"))))
-  (define semantics-ghost (launder (ghost (make-cloud "semantics"))))
-  (define syntax-cloud (cc-superimpose the-cloud syntax))
-  (define semantics-cloud (cc-superimpose the-cloud semantics))
-  (define base (fade-to-ghost PLT-LOGO-PICT n1))
-  (define syntax-slide (fade-from-ghost syntax-cloud n1))
-  (define semantics-slide (fade-from-ghost semantics-cloud n1))
-  (slide-pict/center
-   (slide-pict/center
-    (ppict-do
-     my-base-pict
+ (λ (n1 n2 n3 n4 n5 n6)
+   (define (word-cloud label)
+     (ppict-do
+      the-cloud
+      #:go (coord .5 .33)
+      (fade-from-ghost (my-t label) n1)))
+   (define center-cloud-pos (launder (ghost the-cloud)))
+   (define reader-cloud-pos (launder (ghost the-cloud)))
+   (define runtime-cloud-pos (launder (ghost the-cloud)))
+   (define transformer-cloud-pos (launder (ghost the-cloud)))
+   (define the-runtime-cloud (runtime-cloud n1 n2 n3 n4 n5 n6))
+   (define the-transformer-cloud (transformer-cloud n1 n2 n3 n4 n5 n6))
+   (define the-reader-cloud (reader-cloud n1 n2 n3 n4 n5 n6))
+   (define lang-pict
+     (fold-fade
+      (map
+       (λ (str) (my-tt (string-append "#lang " str)))
+       '("" "racket" "typed/racket" "scribble" "mini-java" "racket"))
+      #:combine lc-superimpose
+      n2
+      n3
+      n4
+      n5
+      n6))
+   (define lang-start (launder (ghost lang-pict)))
+   (define lang-end (launder (ghost lang-pict)))
+   (define base-pict
+     (ppict-do
+      my-base-pict
+      #:go (coord 0 0 'lt)
+      lang-end
       #:go (coord .5 .5)
-      base
-      #:go (coord .5 .1)
-      (fade-to-ghost (titlet "The Racket Approach to Programming Languages") n1)
-      #:go (tile 2 1)
-      syntax-ghost
-      semantics-ghost
-      #:go (coord .49 .1 'rc)
-      (fade-from-ghost (my-tt "#lang") n2))
-    syntax-slide
-    base
-    syntax-ghost
-    n1)
-   semantics-slide
-   base
-   semantics-ghost
-   n1)))
-
-(play-n
- (λ (n1 n2 n3 n4 n5)
-   (define langs (apply fold-fade
-                        (map my-tt (list "" "racket" "typed/racket" "scribble" "mini-java" "racket"))
-                        (list n1 n2 n3 n4 n5)
-                        #:combine lc-superimpose))
-   (define empty-cloud (blank WIDTH HEIGHT))
-   (define bindings
-     (fold-fade (list empty-cloud racket-bindings typed-racket-bindings scribble-bindings mj-bindings racket-bindings) n1 n2 n3 n4 n5))
-   (define readings
-     (fold-fade (list empty-cloud racket-readers typed-racket-readers scribble-readers mj-readers racket-readers) n1 n2 n3 n4 n5))
-   (ppict-do
-    my-base-pict
-    #:go (tile 2 1)
-    (cc-superimpose (make-cloud "reader") readings)
-    (cc-superimpose (make-cloud "bindings") bindings)
-    #:go (coord .49 .1 'rc)
-    (my-tt "#lang")
-    #:go (coord .51 .1 'lc)
-    langs))
+      (cc-superimpose
+       center-cloud-pos
+       lang-start)
+      #:go (coord .24 .5)
+      reader-cloud-pos
+      #:go (coord .76 .22)
+      transformer-cloud-pos
+      #:go (coord .76 .78)
+      runtime-cloud-pos))
+   (for/fold ([base base-pict])
+             ([pict (in-list (append (list the-runtime-cloud the-reader-cloud the-transformer-cloud lang-pict)))]
+              [pict-end (in-list (list runtime-cloud-pos
+                                       reader-cloud-pos
+                                       transformer-cloud-pos
+                                       lang-end))])
+     (slide-pict/center base pict center-cloud-pos pict-end n1)))
  #:skip-last? #t)
+(start-at-recent-slide)
+(play-n
+ (λ (n1)
+   (define lang (my-tt "#lang racket"))
+   (define lang-start (launder (ghost lang)))
+   (define lang-end (launder (ghost lang)))
+   (define nothing (blank 0 0))
+   (slide-pict
+    (slide-picts/tags
+     (ppict-do
+      my-base-pict
+      #:go (coord 0 0 'lt) lang-start
+      #:go (coord .5 0 'ct) lang-end
+      #:go (coord .24 .5) (tg nothing 'reader-start)
+      #:go (coord -0.4 -0.5) (tg nothing 'reader-end)
+      #:go (coord .76 .22) (tg nothing 'transformer-start)
+      #:go (coord .5 .5) (tg nothing 'transformer-end)
+      #:go (coord .76 .78) (tg nothing 'runtime-start)
+      #:go (coord 1.42 1.78) (tg nothing 'runtime-end))
+     (list final-reader
+           final-runtime
+           final-transformer)
+     '(reader-start runtime-start transformer-start)
+     '(reader-end runtime-end transformer-end)
+     n1)
+    lang
+    lang-start
+    lang-end
+    n1
+    )))
+
+
+
+
+
 
 
 (play-n
@@ -151,7 +184,7 @@
  #:skip-last? #t)
 
 (play-n
- (λ (n1)
+ (λ (n1 n2)
    (slide-picts/tags
     (ppict-do
      my-base-pict
@@ -161,18 +194,25 @@
      (titlet "An Open Compiler")
      #:go (coord .5 .5)
      (fade-to-ghost (compiler #t #t) n1)
+     #:go (coord .25 .9)
+     (fade-from-ghost (t "Racket") n1)
+     #:go (coord .75 .9)
+     (fade-from-ghost (t "MiniJava") n2)
      #:go (tile 2 1)
      (cc-superimpose
-      (fade-from-ghost (file-icon 450 500 "bisque") n1)
+      (fade-from-ghost (file-icon 450 525 "bisque") n1)
       (vl-append
-       30
+       50
+       (ghost (make-code-pict "" ""))
        (tg (ghost (compiler-define #t)) 'defl-e)
        (tg (ghost (compiler-lambda #t)) 'laml-e)
        (tg (ghost (compiler-app #t)) 'appl-e)
        (tg (ghost (compiler-letrec #t)) 'letl-e)
        (tg (ghost (compiler-if #t)) 'ifl-e)
        (tg (ghost (compiler-or #t)) 'orl-e)))
-     (ghost (file-icon 450 500 "bisque")))
+     (cc-superimpose
+      (fade-from-ghost (file-icon 450 525 "Lavender") n2)
+      mj-compiler))
     (list (compiler-define #f)
           (compiler-lambda #f)
           (compiler-app #f)
@@ -181,8 +221,48 @@
           (compiler-or #f))
     '(defl laml appl letl ifl orl)
     '(defl-e laml-e appl-e letl-e ifl-e orl-e)
-    n1)))
-                              
+    n1))
+ #:skip-last? #t)
+
+(play-n
+ (λ (n1 n2 n3)
+   (slide-pict/tags
+    (slide-picts/tags
+     (ppict-do
+      my-base-pict
+      #:go (coord .5 .5)
+      (scale the-cloud 4)
+      #:go (coord .5 .1)
+      (titlet "An Open Compiler")
+      #:go (coord .25 .9)
+      (t "Racket")
+      #:go (coord .75 .9)
+      (t "MiniJava")
+      #:go (tile 2 1)
+      (cc-superimpose
+       (file-icon 450 525 "bisque")
+       (vl-append
+        50
+        (ghost (make-code-pict "" ""))
+        (tg (compiler-define #f) 'defl-e)
+        (tg (compiler-lambda #f) 'laml-e)
+        (tg (compiler-app #f) 'appl-e)
+        (tg (compiler-letrec #f) 'letl-e)
+        (tg (compiler-if #f) 'ifl-e)
+        (tg (compiler-or #f) 'orl-e)))
+      (cc-superimpose
+       (file-icon 450 525 "Lavender")
+       mj-compiler))
+     (list
+      (backdrop (inset (compiler-if-mj #f) 10 15) #:color "bisque")
+      (backdrop (inset (colored-or n2) 10 15) #:color "bisque"))
+     '(ifl-e orl-e)
+     '(mj-if-e mj-or-e)
+     n1)
+    (compiler-while #f)
+    'mj-while-s
+    'mj-while-e
+    n3)))
 
    
 
--- talk/util.rkt
+++ talk/util.rkt
@@ -1,22 +1,22 @@
 #lang racket
 (require pict images/logos slideshow pict/code ppict)
+(require (prefix-in : (only-in pict/code code)))
 
 (provide (all-defined-out))
 
-(define HEIGHT 450)
-(define WIDTH 450)
+(define WIDTH 500)
+(define HEIGHT 375)
 (define RADIUS 0.5)
 (define INNER-DIAMETER 340)
 (define INNER-RADIUS (/ INNER-DIAMETER 2))
 (define RATIO (/ INNER-DIAMETER WIDTH))
 
-(define PLT-LOGO-PICT
-  #;(blank client-w client-h)
-  (freeze (bitmap (plt-logo #:height 450))))
+(define (my-t label)
+  (text label (current-main-font) (* 2 (current-font-size))))
 
 (define (make-cloud label #:color [color "AliceBlue"] #:size [size (current-font-size)])
-  (define background-cloud (cloud (+ WIDTH 4) (+ HEIGHT 4) "black"))
-  (define the-cloud (cloud WIDTH HEIGHT color))
+  (define background-cloud (cloud WIDTH HEIGHT "black"))
+  (define the-cloud (cloud (- WIDTH 4) (- HEIGHT 4) color))
   (define the-label (text label (current-main-font) (* 2 size)))
   (cc-superimpose background-cloud the-cloud the-label))
 
@@ -25,47 +25,29 @@
   (text str (current-main-font) (* 2 (current-font-size))))
 
 (define the-cloud
-  (cc-superimpose (cloud (+ WIDTH 4) (+ HEIGHT 4) "black")
-                  (cloud WIDTH HEIGHT "AliceBlue")))
+  (cc-superimpose (cloud WIDTH HEIGHT "black")
+                  (cloud (- WIDTH 4) (- HEIGHT 4) "AliceBlue")))
 
 (define (my-tt str)
          (text str `(bold . modern)  (* 1.5 (current-font-size))))
 
+(define SLIDE-W 1024)
+(define SLIDE-H 768)
+
 (define my-base-pict
-  (colorize (filled-rectangle client-w client-h) "Ivory"))
+  (inset
+   (colorize (filled-rectangle SLIDE-W SLIDE-H) "Ivory")
+   (- client-w SLIDE-W)
+   (- client-h SLIDE-H)))
 
-#;(define (lang-pict lang reader-exs binding-exs)
-  (define (my-tt str)
-    (text str `(bold . modern)  (* 1.5 (current-font-size))))
-  (define (make-code-pict str #:scale [s 1.5])
+(define CODE-SCALE 1.85)
+(define-syntax-rule (code . stx)
+  (parameterize ([code-colorize-enabled #f])
     (scale
-     (codeblock-pict #:keep-lang-line? #f
-                     (string-append "#lang " lang "\n" str))
-     s))
-  (match-define (list r1 r2) (map make-code-pict reader-exs))
-  (match-define (list b1 b2 b3) (map make-code-pict binding-exs))
-  (pslide
-   #:go (tile 2 1)
-   (ppict-do
-    (make-cloud "reader")
-    #:go (coord .5 .33)
-    r1
-    #:go (coord .5 .75)
-    r2)
-   (ppict-do
-    (make-cloud "bindings")
-    #:go (coord .25 .33)
-    (rotate b1 (/ pi 6))
-    #:go (coord .75 .33)
-    (rotate b2 (- (/ pi 6)))
-    #:go (coord .5  .75)
-    b3)
-   #:go (coord .51 .1 'lc)
-   (my-tt lang)
-   #:go (coord .49 .1 'rc)
-   (my-tt "#lang")))
+     (:code . stx)
+     CODE-SCALE)))
 
-(define (make-code-pict lang str #:scale [s 1.85])
+(define (make-code-pict lang str #:scale [s CODE-SCALE])
     (define the-lang
       (if (string=? "" lang) "racket" lang))
     (scale
@@ -115,6 +97,13 @@
              [time (in-list times)])
     (fade-pict time base pict #:combine combine)))
 
+(define (fold-fade/align picts aligner times)
+  (define base-pict (first picts))
+  (for/fold ([base base-pict])
+            ([pict (in-list (rest picts))]
+             [time (in-list times)])
+    (fade-pict time base pict #:combine aligner)))
+
 
 (define (make-bindings-pict lang words [radii #f] [aligns #f] #:tag? [tag? #f])
   (define picts (map (λ (str) (make-code-pict lang str)) words))
@@ -152,7 +141,7 @@
 (define mj-bindings
   (make-bindings-pict
    "racket"
-   (list "while" "or" "if" "define-class" "new" "send")
+   (list "while" "||" "if" "define-class" "new" "send")
    (list 1 .9 .9 1 1 .9)))
 
 ;; READER PICTS
@@ -190,8 +179,13 @@
 
 
 (define (slide-pict/tags base p tag-from tag-to n #:find [find cc-find])
+  ;(printf "n: ~a\n" n)
+  ;(printf "from: ~a\n" tag-from)
+  ;(printf "to: ~a\n" tag-to) 
   (define pict-path-from (find-tag base tag-from))
+  ;(printf "pict-path-from : ~a\n" pict-path-from)
   (define pict-path-to (find-tag base tag-to))
+  ;(printf "pict-path-to : ~a\n" pict-path-to)
   (define-values (x-from y-from) (find base pict-path-from))
   (define-values (x-to y-to) (find base pict-path-to))
   (pin-over/align
@@ -220,7 +214,7 @@
     [(pict? str) str]
     [else (make-code-pict "" str)]))
 
-(define (make-tagged-ghost str [ghost? #t] [post-tag "-end"])
+(define (make-tagged-ghost str [ghost? #t] #:tag-suffix [post-tag "-end"])
   (define do-ghost (if ghost? ghost values))
   (tag-pict (do-ghost (make-code-pict "" str)) (string->symbol (string-append str post-tag))))
 
@@ -261,3 +255,156 @@
   (map
    (λ (str) (string->symbol (string-append str "-end")))
    (list "define" "lambda" "#%app" "letrec" "if" "or")))
+
+(define (make-racket-pict str [colorize-enabled #t])
+  (parameterize ([code-colorize-enabled colorize-enabled])
+    (make-code-pict "" str)))
+
+(define (compiler-define-class ghost-binding?)
+  (make-compiler-line "[(" (make-tagged-ghost "define-class" ghost-binding? #:tag-suffix "mj") " c b ...) «code»]"))
+(define (compiler-new ghost-binding?)
+  (make-compiler-line "[(" (make-tagged-ghost "new" ghost-binding? #:tag-suffix "mj") " c)                «code»]"))
+(define (compiler-send ghost-binding?)
+  (make-compiler-line "[(" (make-tagged-ghost "send" ghost-binding? #:tag-suffix "mj") " o m a ...)       «code»]"))
+(define (compiler-while ghost-binding?)
+  (make-compiler-line "[(" (make-tagged-ghost "while" ghost-binding? #:tag-suffix "mj") " test body ...)  «code»]"))
+(define (compiler-if-mj ghost-binding?)
+  (make-compiler-line "[(" (make-tagged-ghost "if" ghost-binding? #:tag-suffix "mj") " test then else)    «code»]"))
+(define (compiler-or-mj ghost-binding?)
+  (make-compiler-line "[(" (make-tagged-ghost "or" ghost-binding? #:tag-suffix "mj") " e1 e2 ...)         «code»]"))
+
+(require unstable/gui/pict)
+
+(define (colored-or n)
+  (make-compiler-line "[("
+                      (fade-pict n
+                                 (make-racket-pict "or")
+                                 (backdrop (make-racket-pict "||") #:color "Lavender"))
+                      " e1 e2 ...)"
+                      "         «code»]"))
+
+(define (pin-over* base dx dy picts)
+  (for/fold ([base base])
+            ([pict (in-list picts)])
+    (pin-over base dx dy pict)))
+
+(define mj-compiler
+  (pin-over*
+   (vl-append
+    50
+    (ghost (make-code-pict "" ""))
+    (tg (ghost (compiler-define-class #f)) 'mj-def-class-e)
+    (tg (ghost (compiler-send #f)) 'mj-send-e)
+    (tg (ghost (compiler-new #f)) 'mj-new-e)
+    (tg (ghost (compiler-while #f)) 'mj-while-e)
+    (tg (ghost (compiler-if-mj #f)) 'mj-if-e)
+    (tg (ghost (compiler-or-mj #f)) 'mj-or-e))
+   0
+   -200 ; FIXME: what's the right number here???
+   (list (tg (ghost (compiler-define-class #f)) 'mj-def-class-s)
+         (tg (ghost (compiler-send #f)) 'mj-send-s)
+         (tg (ghost (compiler-new #f)) 'mj-new-s)
+         (tg (ghost (compiler-while #f)) 'mj-while-s))))
+
+
+(define (word-cloud label picts)
+  (define label-pict (if (pict? label) label (my-t label)))
+  (match-define (list p1 p2 p3 p4 p5 p6) picts)
+  (ppict-do
+   the-cloud
+   #:go (coord .5 .33) label-pict
+   #:go (coord .5 .17)
+   (ppict-do
+    (blank 220 80)
+    #:go (coord .5 .5 'cc) p1)
+   #:go (coord .5 .6)
+   (ppict-do
+    (blank 440 145)
+    #:go (coord 0 .25 'lc) p2
+    #:go (coord 1 .1 'rc) p3
+    #:go (coord 0.2 0.5 'lc) p4
+    #:go (coord 1 .75 'rt) p5)
+   #:go (coord .5 .9)
+   (ppict-do
+    (blank 200 60)
+    #:go (coord .5 .5 'cb) p6)))
+
+(define CODE-BLOB-PICT (make-racket-pict "«code»"))
+
+(define (make-code-map-pict pict)
+  (define OPACITY .75)
+  (define SCALE .75)
+  (hc-append (make-racket-pict "⟨")
+             pict
+             (make-racket-pict " ↦ ")
+             (scale (cellophane CODE-BLOB-PICT OPACITY) SCALE)
+             (make-racket-pict "⟩")))
+
+
+(define (fade-to-ghost p n)
+  (fade-pict n p (ghost p)))
+(define (fade-from-ghost p n)
+  (fade-pict n (ghost p) p))
+
+(define (transformer-cloud . times)
+  (word-cloud-transition
+    (fade-from-ghost (label "transformer") (first times))
+    (list "racket" "typed/racket" "scribble" "racket" "racket")
+    (list (list "letrec" "or" "if" "define" "lambda" "#%app")
+          (list "All" ":" "ann" "define-type" "require/typed" "Listof")
+          (list "defproc" "examples" "authors" "define-cite" "include-section" "code")
+          (list "while" "||" "if" "define-class" "send" "new")
+          (list "letrec" "or" "if" "define" "lambda" "#%app"))
+    (rest times)))
+
+(define (reader-cloud . times)
+  (word-cloud-transition #:reader? #t
+    (fade-from-ghost (label "reader") (first times))
+    (list "racket" "typed/racket" "scribble" "mini-java" "racket")
+    (list (list "#(1 2 3)" "(4 . < . 5)" "#hash()" "'(6 7 . 8)" "`(,(+ 9 10))" "#i10")
+          (list "'(1 2)" "#{2 :: Integer}" "#{map @ String Integer}" "" "" "")
+          (list "@~cite[plt-tr1]" "@title[#:tag \"mj\"]{Intro}" "" "@{Hello}"  "" "" )
+          (list  "check.is_even(6)" "true || false" "class Main {...}" "" "" "")
+          (list "#(1 2 3)" "(4 . < . 5)" "'(6 7 . 8)" "#hash()" "`(,(+ 9 10))" "#i10"))
+    (rest times)))
+
+(define (runtime-cloud . times)
+  (word-cloud-transition
+    (fade-from-ghost (label "runtime") (first times))
+    (list "racket" "typed/racket" "scribble" "racket" "racket")
+    (list (list "empty?" "+" "<=" "read" "vector" "cons")
+          (list "empty?" "+" "<=" "read" "vector" "cons")
+          (list "secref" "title" "cite" "figure" "make-bib" "bold")
+          (list "index" "+" "!" "new-int-array" "length" "==")
+          (list "empty?" "+" "<=" "read" "vector" "cons"))
+    (rest times)))
+
+(define (transpose lst) (apply map list lst))
+
+(define (word-cloud-transition label langs wordss #:reader? [reader? #f] times)
+  (define SCALE 2.25)
+  (define ALIGNERS (list cc-superimpose lc-superimpose rc-superimpose lc-superimpose rt-superimpose cb-superimpose))
+  (define prepare-pict (if reader? values make-code-map-pict))
+  (define base (ppict-do the-cloud #:go (coord .5 .33) label))
+  (define picts
+    (for/list ([words (in-list (transpose wordss))]
+               [aligner (in-list ALIGNERS)])
+      (fade-from-ghost
+       (fold-fade/align
+        (for/list ([lang (in-list langs)]
+                   [word (in-list words)])
+          (prepare-pict (make-code-pict lang word #:scale SCALE)))
+        aligner
+        (rest times))
+       (first times))))
+  (word-cloud label picts))
+
+(define final-reader (reader-cloud 1 1 1 1 1 1))
+(define final-transformer (transformer-cloud 1 1 1 1 1 1))
+(define final-runtime (runtime-cloud 1 1 1 1 1 1))
+
+
+
+             
+             
+  
